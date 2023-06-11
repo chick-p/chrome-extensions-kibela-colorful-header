776393a979ed73d229360d60f951885f3370c375
piyo <chick.p.1120@gmail.com>
2023-06-11 19:37:57 +0900

    feat: set icons

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ const manifest = defineManifest({
   description:  "This is an example extension",
   name: "example",
   version: "0.1.0",
+  icons: {
+    16: "icons/icon16.png",
+    32: "icons/icon32.png",
+    128: "icons/icon128.png",
+  },
 });
 
 export default defineConfig({
